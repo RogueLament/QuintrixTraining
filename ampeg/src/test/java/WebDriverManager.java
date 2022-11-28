e8f05d983be6269ac6d678a8095b3a0993d9b2f3
RogueLament <45111295+RogueLament@users.noreply.github.com>
2022-11-27 23:26:20 -0800

    Implemented the WebDriver, added Edge and Chrome functionality

---
+++ ampeg/src/test/java/WebDriverManager.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class WebDriverManager {
+	protected WebDriver driver;
+	protected abstract void startService() throws IOException;
+	protected abstract void stopService();
+	protected abstract void createDriver();
+	
+	public void quitDriver() {
+		if (driver != null) {
+			driver.quit();
+			driver = null;
+		}
+	}
+	
+	public WebDriver getDriver() throws IOException {
+		if (driver == null) {
+			startService();
+			createDriver();
+		}
+		return driver;
+	}
+}
