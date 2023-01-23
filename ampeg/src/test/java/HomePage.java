b790cc22f3aed98a4b64a1dab9d843a51ff59503
RogueLament <45111295+RogueLament@users.noreply.github.com>
2023-01-23 10:10:53 -0800

    Fixed homepage so it works

--- ampeg/src/test/java/HomePage.java
+++ ampeg/src/test/java/HomePage.java
@@ -3,32 +3,40 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage extends PageObject {
 	public final String uRL = "index.html";
-	
+
 	public HomePage(WebDriver driver, String baseURL) {
 		super(driver, baseURL);
 		setURL(uRL);
 	}
-	
+
 	public HomePage navigateDirect() {
 		navigateDirectURL();
-		
+
 		return this;
 	}
-	
+
+	public HomePage clickShop() {
+		var shopLink = this.driver.findElement(By.xpath("//a[@title='Shop']"));
+
+		shopLink.click();
+
+		return this;
+	}
+
 	public HomePage clickSearch() {
 		var searchLink = this.driver.findElement(By.xpath("//a[@title='Search']"));
-		
+
 		searchLink.click();
-		
+
 		return this;
 	}
-	
+
 	public HomePage search(String query) {
 		var searchBox = this.driver.findElement(By.id("searchq"));
-		
+
 		searchBox.sendKeys(query);
 		searchBox.submit();
-		
+
 		return this;
 	}
 }
