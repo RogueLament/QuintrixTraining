d3a5be04cf87321173663b8a1b7189df8fec6996
RogueLament <45111295+RogueLament@users.noreply.github.com>
2023-01-23 11:51:59 -0800

    Completed, second search test fails purposely to test onTestFail listener.

--- ampeg/src/test/java/ListenerTest.java
+++ ampeg/src/test/java/ListenerTest.java
@@ -1,26 +1,7 @@
-import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class ListenerTest implements ITestListener {
-	@Override
-	public void onFinish(ITestContext Result) 					
-	{
-
-	}
-
-	@Override
-	public void onStart(ITestContext Result)					
-	{
-
-	}
-
-	@Override
-	public void onTestFailedButWithinSuccessPercentage(ITestResult Result)					
-	{
-
-	}
-
 	@Override
 	public void onTestFailure(ITestResult Result) 					
 	{
